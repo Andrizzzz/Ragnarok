abde5fcb86c35c79fc36220f3a262f2c3f3776b3
IANGAMINGPC\Ian <ianyniegocho3020@gmail.com>
2024-04-21 12:39:50 +0800

    0-21-2024 update: added three game slots but it still has problems

--- Assets/Arts/Script/Quit.cs
+++ Assets/Arts/Script/Quit.cs
@@ -13,16 +13,18 @@ public class Quit : MonoBehaviour
     }
     public void QuitGame()
     {
-        PlayerPrefs.SetInt("LoadSaved", 1);
-        PlayerPrefs.SetInt("SavedScene", SceneManager.GetActiveScene().buildIndex);
+        PlayerPrefs.SetInt("SlotSaved" + SaveID.saveID, 1);
+        PlayerPrefs.SetInt("LoadSaved" + SaveID.saveID, 1);
+        PlayerPrefs.SetInt("SavedScene" + SaveID.saveID, SceneManager.GetActiveScene().buildIndex);
         playerPosData.PlayerPosSave();
         SceneManager.LoadScene("Main Menu");
         Time.timeScale = 1;
     }
     public void Save()
         {
-        PlayerPrefs.SetInt("LoadSaved", 1);
-        PlayerPrefs.SetInt("SavedScene", SceneManager.GetActiveScene().buildIndex);
+        PlayerPrefs.SetInt("SlotSaved" + SaveID.saveID, 1);
+        PlayerPrefs.SetInt("LoadSaved" + SaveID.saveID, 1);
+        PlayerPrefs.SetInt("SavedScene" + SaveID.saveID, SceneManager.GetActiveScene().buildIndex);
         playerPosData.PlayerPosSave();
         }
 }
