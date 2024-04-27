868dbc52be1d87b43d817cb4e3de17aab0578c94
Andrizz <lanceandri@gmail.com>
2024-04-27 18:21:28 +0800

    04-27-24 Updates: Added: PlayerDetector, Added Script

---
+++ Assets/Scripts/Enemy/States/IdleState.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : State
+{
+    protected D_IdleState stateData;
+
+    protected bool flipAfterIdle;
+    protected bool isIdleTimeOver;
+    protected bool isPlayerInMinAgroRange;
+
+    protected float IdleTime;
+
+    public IdleState(Entity entity, FiniteStateMachine stateMachine, string animBool, D_IdleState stateData) : base(entity, stateMachine, animBool)
+    {
+        this.stateData = stateData;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        entity.SetVelocity(0f);
+        isIdleTimeOver = false;
+        SetRandomIdleTime();
+        isPlayerInMinAgroRange = entity.CheckPlayerInMinAgroRange();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        if (flipAfterIdle)
+        {
+            entity.Flip();
+        }
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        if (Time.time >= startTime + IdleTime)
+        {
+            isIdleTimeOver = true;
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+        isPlayerInMinAgroRange = entity.CheckPlayerInMinAgroRange();
+
+    }
+
+    public void SetFlipAfterIdle(bool flip)
+    {
+        flipAfterIdle = flip;
+    }
+
+    private void SetRandomIdleTime()    
+    {
+        IdleTime = Random.Range(stateData.minIdleTime, stateData.maxIdleTime);
+    }
+
+}
