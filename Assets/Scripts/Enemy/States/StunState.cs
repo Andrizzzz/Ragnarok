dc0dadae1099cd92266b9833b6ef0d62799ed5e9
Andrizz <lanceandri@gmail.com>
2024-04-30 11:18:58 +0800

    SAVE FOR LATER

---
+++ Assets/Scripts/Enemy/States/StunState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunState : State
+{
+public StunState(Entity entity, FiniteStateMachine stateMachine, string animBool) : base(entity, stateMachine, animBool)
+    {
+       this.stateData = stateData;
+    }
+    
+}
