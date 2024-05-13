6f83f0fe84c16a2e304de581afc4139b1226fbed
IANGAMINGPC\Ian <ianyniegocho3020@gmail.com>
2024-05-14 06:09:29 +0800

    SAVE

---
+++ Assets/Arts/Script/Inventory.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lance
+{
+    public class Inventory : MonoBehaviour {
+
+
+        public bool[] isFull;
+            public GameObject[] slots;
+
+    }
+}
