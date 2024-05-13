544ab838d390897dc41a002def9c0e30d3e8a661
Andrizz <lanceandri@gmail.com>
2024-05-14 03:03:16 +0800

    Save Fix All Bug

--- Assets/Scripts/ScriptableObject/WeaponDataSO.cs
+++ Assets/Scripts/ScriptableObject/WeaponDataSO.cs
@@ -6,22 +6,33 @@ using UnityEngine;
 
 namespace Lance.Weapons.Components
 {
-    [CreateAssetMenu(fileName = "newWeaponData", menuName = "Data/Weapon Data/Weapon")]
+    [CreateAssetMenu(fileName = "newWeaponData", menuName = "Data/Weapon Data/Basic Weapon Data", order = 0)]
     public class WeaponDataSO : ScriptableObject
     {
+        [field: SerializeField] public Sprite Icon { get; set; }
+        [field: SerializeField] public string Name { get; private set; }
+        [field: SerializeField] public string Description { get; private set; }
+        [field: SerializeField] public RuntimeAnimatorController AnimatorController { get; private set; }
         [field: SerializeField] public int NumberOfAttacks { get; private set; }
 
-        [field: SerializeReference]public List<ComponentData> ComponentData { get; private set; }
+        [field: SerializeReference] public List<ComponentData> ComponentData { get; private set; }
 
         public T GetData<T>()
         {
             return ComponentData.OfType<T>().FirstOrDefault();
         }
 
-        [ContextMenu("Add Sprite Data")]
-        private void AddSpriteData() => ComponentData.Add(new WeaponSpriteData());
+        //public List<Type> GetAllDependencies()
+        //{
+        //    return ComponentData.Select(component => component.ComponentDependency).ToList();
+        //}
 
-        [ContextMenu("Add Movement Data")]
-        private void AddMovementData() => ComponentData.Add(new MovementData());
+        public void AddData(ComponentData data)
+        {
+            if (ComponentData.FirstOrDefault(t => t.GetType() == data.GetType()) != null)
+                return;
+
+            ComponentData.Add(data);
+        }
     }
 }
