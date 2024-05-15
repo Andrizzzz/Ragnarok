fa4177403dcb2028c00411463d8362021968fdb6
IANGAMINGPC\Ian <ianyniegocho3020@gmail.com>
2024-05-16 03:06:23 +0800

    Update: scriptable objects in progress.

---
+++ Assets/Arts/Script/Inventory/ItemSO.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lance
+{
+    [CreateAssetMenu]
+    public class ItemSO : ScriptableObject
+    {
+        public string itemName;
+
+
+
+
+
+
+        public enum StatToChange
+
+}
