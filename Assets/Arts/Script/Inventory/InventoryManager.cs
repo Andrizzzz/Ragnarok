251aded3f6592a27dbd9be1bbe07f78830dd7893
IANGAMINGPC\Ian <ianyniegocho3020@gmail.com>
2024-05-15 10:53:03 +0800

    Inventory(SAVE)

--- Assets/Arts/Script/Inventory/InventoryManager.cs
+++ Assets/Arts/Script/Inventory/InventoryManager.cs
@@ -46,6 +46,16 @@ namespace Lance
                 }
             }
         }
+
+
+        public void DeselectAllSlots()
+        {
+            for (int i = 0; i < itemSlot.Length; i++)
+            {
+                itemSlot[i].selectedShader.SetActive(false);
+                itemSlot[i].thisItemSelected = false;
+            }
+        }
     }
 }
 
--- Assets/Arts/Script/Inventory/ItemSlot.cs
+++ Assets/Arts/Script/Inventory/ItemSlot.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems; // Add this directive
 
 namespace Lance
 {
-    public class ItemSlot : MonoBehaviour
+    public class ItemSlot : MonoBehaviour, IPointerClickHandler
     {
         public string itemName;
         public int quantity;
@@ -18,6 +19,22 @@ namespace Lance
 
         [SerializeField]
         private Image itemImage;
+
+        public GameObject selectedShader;
+        public bool thisItemSelected;
+
+        private InventoryManager inventoryManager;
+
+        private void Start()
+        {
+            // Find the InventoryManager in the scene and assign it to the inventoryManager field
+            inventoryManager = GameObject.FindObjectOfType<InventoryManager>();
+
+            if (inventoryManager == null)
+            {
+                Debug.LogError("InventoryManager not found in the scene.");
+            }
+        }
 
         public void AddItem(string itemName, int quantity, Sprite itemsprite)
         {
@@ -40,6 +57,36 @@ namespace Lance
             quantityText.text = this.quantity.ToString();
             quantityText.enabled = true;
             itemImage.sprite = this.itemSprite;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (eventData.button == PointerEventData.InputButton.Left)
+            {
+                OnLeftClick();
+            }
+            if (eventData.button == PointerEventData.InputButton.Right)
+            {
+                OnRightClick();
+            }
+        }
+
+        public void OnLeftClick()
+        {
+            if (inventoryManager == null)
+            {
+                Debug.LogError("InventoryManager is not assigned.");
+                return;
+            }
+
+            inventoryManager.DeselectAllSlots();
+            selectedShader.SetActive(true);
+            thisItemSelected = true;
+        }
+
+        public void OnRightClick()
+        {
+            // Implement right-click functionality here
         }
     }
 }
