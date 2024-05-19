450135a6f5ea5204e8434e3e70b1b994891e4564
Andrizz <lanceandri@gmail.com>
2024-05-19 22:15:50 +0800

    SAVE

---
+++ Assets/Scripts/Weapons/Components/KnockBack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
