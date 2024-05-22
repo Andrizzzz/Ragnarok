a617650d37b559ae513b77e0494c06332bae76f5
Andrizz <lanceandri@gmail.com>
2024-05-23 04:19:07 +0800

    Save

--- Assets/Resourcess/Quests/Dialogue/DialogueSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewDialogue", menuName = "ScriptableObjects/DialogueSO", order = 1)]
-public class DialogueSO : ScriptableObject
-{
-    [TextArea(3, 10)]
-    public string[] dialogueLines;
-}
