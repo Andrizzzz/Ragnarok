fe8062a7f3d1144da6cc8be26e5c665af476305f
Andrizz <lanceandri@gmail.com>
2024-05-29 04:14:46 +0800

    05 - 29 - 24Updates: Added Enemy5

--- Assets/Arts/Script/Lazer.cs
+++ Assets/Arts/Script/Lazer.cs
@@ -1,28 +1,39 @@
+using Lance.Weapons.Components;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Lazer : MonoBehaviour
 {
-    private float timeTilSpawn;
-
-
-
-    public float startTimeTilSpawn;
-
     public GameObject lazer;
     public Transform whereToSpawn;
 
-    private void Update()
+    private void Start()
     {
-        if (timeTilSpawn <= 0)
+        StartCoroutine(ToggleLazer());
+    }
+
+    private IEnumerator ToggleLazer()
+    {
+        while (true)
         {
+            lazer.SetActive(false);
+            yield return new WaitForSeconds(2f);
             Instantiate(lazer, whereToSpawn.position, whereToSpawn.rotation);
-            timeTilSpawn = startTimeTilSpawn;
+            lazer.SetActive(true);
+            yield return new WaitForSeconds(2f);
         }
-        else
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
         {
-            timeTilSpawn -= Time.deltaTime;
+            var damageable = other.GetComponent<IDamageable>();
+            if (damageable != null)
+            {
+                damageable.Damage(10); // or any other amount you want to deal
+            }
         }
     }
 }
